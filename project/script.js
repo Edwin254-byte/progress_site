c7d3d5d2848a727f2dc6aa4ae2239639b6733ac9
EDWIN <84503319+Edwin254-byte@users.noreply.github.com>
2022-06-15 06:34:46 +0300

    finished first 3 sections of about us page

--- project/script.js
+++ project/script.js
@@ -1,4 +1,4 @@
-function reveal() {
+/* function reveal() {
   var reveals = document.querySelector(".reveal");
 
   for (var i = 0; i < reveals.length; i++) {
@@ -13,3 +13,32 @@ function reveal() {
     }
   }
 }
+ */
+
+//reveal effect
+const container = document.getElementById("card-container");
+const cards = document.querySelectorAll(".card");
+const observeCard = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("card--hidden");
+  observer.unobserve(entry.target);
+};
+const cardObserver = new IntersectionObserver(observeCard, {
+  root: null,
+  threshold: 0.2,
+});
+cards.forEach((card) => {
+  cardObserver.observe(card);
+});
+
+/* //alternating effect
+const cardIntros = document.querySelectorAll(".card__intro");
+const cardContents = document.querySelectorAll(".card__content");
+console.log(cardContents);
+console.log(cardIntros);
+cardIntros.forEach((el, i) => {
+  if (i % 2 == 1) {
+    el.innerHTML = "";
+  }
+}); */
